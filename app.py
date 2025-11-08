c5d9eaf967b1f9d0f07763e97e78a69ffdf2401d
Kuah Zhao Yang <zykuah2006@gmail.com>
2025-11-08 05:06:47 -0500

    Add dashboard route and create_slides API call

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask
+# Before running, run "pip install -r requirements.txt" in venv
+
+from flask import Flask, jsonify, request
 from dotenv import load_dotenv
+from python_files.slide_create import main as create_slides
 
 load_dotenv()
 
@@ -9,6 +12,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'kpop demon hunters RAHHHHHHHHH!'
+
+@app.route('/create_slides', methods=['POST'])
+def create_slides_route():
+    data = request.json
+    text = data.get('text')
+    
+    if not text:
+        return jsonify({'error': 'Text is required'}), 400
+    
+    slides = create_slides(text)
+    return jsonify({'slides': slides})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
