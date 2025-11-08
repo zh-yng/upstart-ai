3d5e7eb2c35bf0b57dbf445581b193966882d45b
Kuah Zhao Yang <zykuah2006@gmail.com>
2025-11-08 01:18:33 -0500

    Add primereact ui with lara dark indigo <3

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { PrimeReactProvider } from 'primereact/api';
+
+import "primereact/resources/themes/lara-dark-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+
+const value = {
+  ripple: true,
+};
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PrimeReactProvider value={value}>
+      <App />
+    </PrimeReactProvider>
   </StrictMode>,
 )
