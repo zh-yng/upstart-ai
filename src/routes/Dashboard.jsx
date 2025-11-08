78ab1af4a7f40c172623f6f09bb29bc1ab0661f3
Kuah Zhao Yang <zykuah2006@gmail.com>
2025-11-08 05:06:47 -0500

    Add dashboard route and create_slides API call

---
+++ src/routes/Dashboard.jsx
@@ -0,0 +1,15 @@
+const Dashboard = () => {
+    return (
+        <>
+            <div className="flex flex-column gap-4 justify-content-center align-items-center text-center" style={{ width: '50%' }}>
+                <div className="flex gap-4 w-full justify-content-center">
+                    <span>Welcome to your dashboard! More features coming soon.</span>
+                    {/* <InputTextarea className="blur" style={{ resize: 'none', width: '100%' }} placeholder="Tell us about your startup idea..." />
+                    <Button icon="pi pi-send" className="p-button-primary" /> */}
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Dashboard;
